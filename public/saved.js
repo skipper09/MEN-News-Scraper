149a864ca7cf0f164ee5ff659df5984b9842cc3a
Sarah Payne <sarah@Sarahs-MacBook-Pro.local>
2017-09-10 12:39:44 -0500

    add modal to saved articles page

--- public/saved.js
+++ public/saved.js
@@ -1,8 +1,22 @@
-$.getJSON("/saved", function(data) {
-    for (var i=0; i<data.length; i++) {
+$(document).on("click", ".comment", function() {
+    $("#myModal").css( "display", "block" );
+})
+
+$(document).on("click", ".close", function() {
+    $("#myModal").css( "display", "none" );
+})
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+$.getJSON("/saved", function (data) {
+    for (var i = 0; i < data.length; i++) {
         $("#saved").append("<p>" + data[i].title + "<br />" + data[i].link + "</p>")
         $("#saved").append("<button data-id='" + data[i]._id + "' class='unsave'>Unsave</button>")
-        $("#saved").append("<button data-id='" + data[i]._id + "' class='comment'>Comment</button>")
+        $("#saved").append("<button data-id='" + data[i]._id + "' class='comment'>View/Leave Comments</button>")
     }
 });
 
@@ -22,3 +36,8 @@ $(document).on("click", ".unsave", function () {
         }
     });
 });
+
+
+
+
+
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require("express"),
     cheerio = require("cheerio"),
     bodyParser = require("body-parser"),
     request = require("request");
-    
+
 mongoose.Promise = Promise;
 
 var Results = require("./models/results.js"),
@@ -19,23 +19,23 @@ app.use(bodyParser.urlencoded({
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/MENnewsScraper");    
+mongoose.connect("mongodb://localhost/MENnewsScraper");
 var db = mongoose.connection
 
-db.on("error", function(error) {
-     console.log("Mongoose error:", error)
+db.on("error", function (error) {
+    console.log("Mongoose error:", error)
 });
 
-db.once("open", function() {
+db.once("open", function () {
     console.log("Mongoose connection successful")
 });
 
 //ROUTES GO HERE
-app.get("/scrape", function(req, res) {
-    request("https://www.reddit.com/r/news/", function(error, response, html) {
+app.get("/scrape", function (req, res) {
+    request("https://www.reddit.com/r/news/", function (error, response, html) {
         var $ = cheerio.load(html);
 
-        $("p.title").each(function(i, element) {
+        $("p.title").each(function (i, element) {
             var result = {};
 
             result.title = $(this).children("a").text();
@@ -43,7 +43,7 @@ app.get("/scrape", function(req, res) {
 
             var entry = new Results(result);
 
-            entry.save(function(err, doc) {
+            entry.save(function (err, doc) {
                 if (err) {
                     console.log(err)
                 } else {
@@ -55,10 +55,9 @@ app.get("/scrape", function(req, res) {
     res.redirect("/");
 })
 
-app.get("/results", function(req, res) {
+app.get("/results", function (req, res) {
+    Results.find({}, function (error, doc) {
 
-    Results.find({}, function(error, doc) {
-        
         if (error) {
             console.log(error)
         } else {
@@ -67,54 +66,86 @@ app.get("/results", function(req, res) {
     });
 });
 
-app.get("/saved", function(req, res) {
-    
-        Saved.find({}, function(error, doc) {
-            
-            if (error) {
-                console.log(error)
-            } else {
-                res.json(doc)
-            }
-        });
+app.get("/saved", function (req, res) {
+    Saved.find({}, function (error, doc) {
+
+        if (error) {
+            console.log(error)
+        } else {
+            res.json(doc)
+        }
     });
+});
+
+app.get("/saved/:id", function (req, res) {
+    Saved.findOne({ "_id": req.params.id }
+        .populate("comment").exec(function (err, doc) {
+            if (err) {
+                console.log(error);
+            } else {
+                res.json(doc);
+            }
+        }));
+});
+
+app.post("/saved/:id", function (req, res) {
+    var newComment = new Comments(req.body);
+
+    newComment.save(function (error, doc) {
+        if (error) {
+            console.log(error)
+        } else {
+            Saved.findOneAndUpdate({ "_id:": req.params.id }, { "comment": doc._id })
+                .exec(function (err, doc) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    else {
+                        // Or send the document to the browser
+                        res.send(doc);
+                    }
+                })
+        }
+
+    })
+})
 
 app.post("/save/:id", function (req, res) {
-    Results.findOne({ "_id": req.params.id}, function(req, res){
-        console.log(res +"kghgjhggfjgfhh");
+    Results.findOne({ "_id": req.params.id }, function (req, res) {
+        console.log(res + "kghgjhggfjgfhh");
 
         var result = {};
         result.title = res.title;
-        result.link = res.link; 
+        result.link = res.link;
 
         var saveArticle = new Saved(result);
 
-        saveArticle.save(function(err, doc) {
+        saveArticle.save(function (err, doc) {
             if (err) {
                 console.log(err)
             } else {
                 console.log(doc + "lkjkjhj")
                 // res.send("Saved article to database")
             }
-        });    
+        });
     })
 });
 
 app.delete("/clear", function (req, res) {
-    Results.remove({}, function(err){
-        if(err) throw err;
+    Results.remove({}, function (err) {
+        if (err) throw err;
     });
 });
 
-app.delete("/unsave/:id", function(req, res) {
-    Saved.remove({_id:req.params.id}, function(err) {
-        if(err) throw err;
+app.delete("/unsave/:id", function (req, res) {
+    Saved.remove({ "_id": req.params.id }, function (err) {
+        if (err) throw err;
         // removed.
     });
 });
 
 // END ROUTES
 
-app.listen(3000, function() {
+app.listen(3000, function () {
     console.log("App running on port 3000")
 });
