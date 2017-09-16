850b8190b01e40a070b78bf41fd39ca38610a84e
Sarah Payne <sarah@Sarahs-MacBook-Pro.local>
2017-09-15 19:50:58 -0500

    adding more style updates

--- public/saved.js
+++ public/saved.js
@@ -1,6 +1,6 @@
 $.getJSON("/saved", function (data) {
     for (var i = 0; i < data.length; i++) {
-        $("#saved").append("<div class='article-div mx-auto'><p>" + data[i].title + "</p><a href='" + data[i].link + "'>" + data[i].link + "</p></a><button data-id='" + data[i]._id + "' class='unsave'>Unsave</button><button data-id='" + data[i]._id + "' class='comment'>View/Leave Comments</button></div>")
+        $("#saved").append("<div class='article-div mx-auto'><p>" + data[i].title + "</p><a href='" + data[i].link + "'>" + data[i].link + "</p></a><span class='float-right clearfix mr-4'><button data-id='" + data[i]._id + "' class='btn btn-sm comment'>View/Leave Comments</button> <button data-id='" + data[i]._id + "' class='unsave btn btn-sm'>Unsave</button></div></span>")
         // $("#saved").append("<button data-id='" + data[i]._id + "' class='unsave'>Unsave</button>")
         // $("#saved").append("<button data-id='" + data[i]._id + "' class='comment'>View/Leave Comments</button></div>")
     }
@@ -21,6 +21,9 @@ $(document).on("click", ".unsave", function () {
             }
         }
     });
+    alert("Article removed");
+    location.reload();
+
 });
 
 $(document).on("click", ".comment", function () {
@@ -39,13 +42,13 @@ $(document).on("click", ".comment", function () {
             $("#modalTitle").append("<h3>Comments for " + JSON.stringify(data.title) + "</h3>");
             if (data.Comments.length > 0) {
                 for (var i = 0; i < data.Comments.length; i++) {
-                    $("#modalComments").append("<p>" + JSON.stringify(data.Comments[i].body) + "<span><button id='deleteComment' data-id='" + data.Comments[i]._id + "' class='delete-button'>Delete</button></span></p>")
+                    $("#modalComments").append("<p>" + JSON.stringify(data.Comments[i].body) + "<span><button id='deleteComment' data-id='" + data.Comments[i]._id + "' class='delete-button btn btn-sm'>Delete</button></span></p>")
                 }
             } else {
                 $("#modalComments").append("<p>No comments yet</p>");
             }
             $("#modalInput").append("<textarea id='bodyinput' name='body' rows='10' cols='60'></textarea>");
-            $("#modalInput").append("<br><button data-id='" + data._id + "' id='leaveComment'>Add Comment</button>")
+            $("#modalInput").append("<br><button data-id='" + data._id + "' id='leaveComment' class='btn-sm btn'>Add Comment</button>")
         });
 
     $("#myModal").css("display", "block");
